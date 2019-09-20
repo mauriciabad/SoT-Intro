d81ec581ac142a7d0c6af2d9bd87207e3a81f928
Maurici <hello@mauriciabad.com>
2019-09-20 14:49:06 +0200

    Week 1 - 4.1 Done

--- client/src/Tester.java
+++ client/src/Tester.java
@@ -10,22 +10,40 @@ import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
 
 public class Tester {
+
     public static void main(String[] args) {
+        test("http://localhost:9090/students");
+        test("http://localhost:9090/students?id=2");
+        test("http://localhost:9090/students/first");
+        test("http://localhost:9090/students/hello");
+        test("http://localhost:9090/students/count");
+        test("http://localhost:9090/students/first");
+    }
+
+    public static int test(String url) {
+        return test(url, "GET", null);
+    }
+
+    public static int test(String url, String method, String body){
         ClientConfig config = new ClientConfig();
         Client client = ClientBuilder.newClient(config);
-        URI baseURI = UriBuilder.fromUri("http://localhost:9090/students").build();
+
+        URI baseURI = UriBuilder.fromUri(url).build();
         WebTarget serviceTarget = client.target(baseURI);
 
         Builder requestBuilder = serviceTarget.path("").request();
         Response response = requestBuilder.get();
 
+        System.out.println();
+        System.out.println(method + " " + url);
+
         if(response.getStatus() == Response.Status.OK.getStatusCode()) {
             String entity = response.readEntity(String.class);
-            System.out.println("OK:");
             System.out.println(entity);
         } else{
-            System.out.println("Error");
             System.out.println(response);
         }
+
+        return response.getStatus();
     }
 }
--- service/src/fontys/sot/rest/service/resources/StudentsResources.java
+++ service/src/fontys/sot/rest/service/resources/StudentsResources.java
@@ -5,6 +5,7 @@ import fontys.sot.rest.service.model.Student;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,15 +16,27 @@ public class StudentsResources {
     private List<Student> studentList = new ArrayList<>();
 
     public StudentsResources() {
-        studentList.add(new Student(1, "Joe Smith"));
-        studentList.add(new Student(2, "Ann Johnsson"));
-        studentList.add(new Student(3, "Miranda Winslet"));
+        // To simplify getters, we create a list of students
+        // where the index and id are the same.
+        studentList.add(new Student(0, "Joe Smith"));
+        studentList.add(new Student(1, "Ann Johnsson"));
+        studentList.add(new Student(2, "Miranda Winslet"));
     }
 
     @GET
     @Produces({MediaType.APPLICATION_JSON})
-    public List<Student> getAllStudents() {
-        return studentList;
+    public List<Student> getStudentByQuery(@QueryParam("id") Integer id) {
+        if (id != null) {
+            if (id < studentList.size()) {
+                List<Student> filteredStudentList = new ArrayList<Student>();
+                filteredStudentList.add(studentList.get(id));
+                return filteredStudentList;
+            } else {
+                throw new RuntimeException("Student wit id " + id + " doesn't exist");
+            }
+        }else{
+            return studentList;
+        }
     }
 
     @GET
